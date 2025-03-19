c768651e5754df45f2d9ccfe26bb0a5ab0ad786d
Difas011 <gabrielbarbosa7651@gmail.com>
2025-03-19 17:16:35 -0300

    Add files via upload

---
+++ projetogym/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+    const navLinks = document.querySelector('.nav-links');
+
+    mobileMenuBtn.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
