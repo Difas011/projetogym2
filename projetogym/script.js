c59b4e1fe400fa729f62cb10ccf8dca2044b302d
Difas011 <gabrielbarbosa7651@gmail.com>
2025-03-25 17:16:33 -0300

    Delete projetogym directory

--- projetogym/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-    const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
-    const navLinks = document.querySelector('.nav-links');
-
-    mobileMenuBtn.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
